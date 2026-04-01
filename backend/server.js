e28717e40632a03bc4978ea54a40f1a9081d5fba
carter.accs <gangbusinessinquires@gmail.com>
2026-03-31 22:49:36 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,12 @@ require("dotenv").config()
 
 const express = require("express")
 const cors = require("cors")
+const path = require("path")
 
 const auth = require("./routes/auth")
 const credits = require("./routes/credits")
+const servers = require("./routes/servers")
+const dashboard = require("./routes/dashboard")
 
 require("./services/scheduler")
 
@@ -13,13 +16,19 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+app.use(express.static("public"))
+
 app.use("/auth", auth)
 app.use("/credits", credits)
+app.use("/servers", servers)
+app.use("/dashboard", dashboard)
 
 app.get("/", (req, res) => {
-    res.send("Pterodactyl Credit System Running")
+    res.sendFile(
+        path.join(__dirname, "public/index.html")
+    )
 })
 
 app.listen(process.env.PORT, () => {
-    console.log("Server running on port " + process.env.PORT)
+    console.log("Running on port " + process.env.PORT)
 })
