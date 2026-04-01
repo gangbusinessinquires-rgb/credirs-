b9decde9a53cda607b7e8fe10eabe592ef52d202
carter.accs <gangbusinessinquires@gmail.com>
2026-03-31 22:41:42 -0400

    Create servers.js

---
+++ backend/routes/servers.js
@@ -0,0 +1,54 @@
+const router = require("express").Router()
+const db = require("../db")
+const ptero = require("../services/pterodactyl")
+
+router.post("/create", async (req, res) => {
+
+    const { discord_id } = req.body
+
+    const user = await db.query(
+        "SELECT * FROM users WHERE discord_id = $1",
+        [discord_id]
+    )
+
+    if (!user.rows.length)
+        return res.send("User not found")
+
+    const username = user.rows[0].username
+
+    const count = await db.query(
+        "SELECT COUNT(*) FROM servers WHERE discord_id = $1",
+        [discord_id]
+    )
+
+    const serverNumber = parseInt(count.rows[0].count) + 1
+
+    const password = `${username}${serverNumber}`
+
+    const server = await ptero.createServer(
+        username,
+        password,
+        serverNumber
+    )
+
+    await db.query(
+        `
+        INSERT INTO servers
+        (discord_id, ptero_server_id, password)
+        VALUES ($1,$2,$3)
+        `,
+        [
+            discord_id,
+            server.id,
+            password
+        ]
+    )
+
+    res.json({
+        success: true,
+        password,
+        server
+    })
+})
+
+module.exports = router
